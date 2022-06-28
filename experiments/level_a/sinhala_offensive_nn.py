aedd41ec647765d3912ff2fb329099a0cde0ab39
Damith Premasiri <damithpremasiri@gmail.com>
2022-06-28 15:39:52 +0100

    remove offensive nn save

--- experiments/level_a/sinhala_offensive_nn.py
+++ experiments/level_a/sinhala_offensive_nn.py
@@ -59,7 +59,6 @@ for i in range(args["n_fold"]):
     predictions, raw_outputs = model.predict(test_sentences)
     test_preds[:, i] = predictions
     print("Completed Fold {}".format(i))
-model.save_model()
 
 final_predictions = []
 for row in test_preds:
