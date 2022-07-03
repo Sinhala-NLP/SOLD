aa0865eb6e292371ccecaddea15db7c3d16f80ac
isuri97 <you@example.com>
2022-07-04 00:29:00 +0530

    change batch size

--- experiments/level_a/sinhala_deepoffense.py
+++ experiments/level_a/sinhala_deepoffense.py
@@ -27,15 +27,15 @@ parser.add_argument('--model_name', required=False, help='model name', default="
 parser.add_argument('--model_type', required=False, help='model type', default="xlmroberta")
 parser.add_argument('--cuda_device', required=False, help='cuda device', default=1)
 parser.add_argument('--train', required=False, help='train file', default='data/olid/olid-training-v1.0.tsv')
-# parser.add_argument('--test', required=False, help='test file', default='data/olid/olid-training-v1.0.tsv')
+parser.add_argument('--test', required=False, help='test file', default='data/olid/olid-training-v1.0.tsv')
 arguments = parser.parse_args()
 
 data = pd.read_csv(arguments.train, sep="\t")
 data = data.rename(columns={'tweet': 'text', 'subtask_a': 'labels'})
 train = data[['text', 'labels']]
 
-train, test = train_test_split(data, test_size=0.2)
-# test= pd.read_csv(arguments.test, sep=",")
+# train, test = train_test_split(data, test_size=0.2)
+test= pd.read_csv(arguments.test, sep=",")
 
 # data = data.rename(columns={'tweet': 'text', 'subtask_a': 'labels'})
 # train = data[['text', 'labels']]
@@ -65,7 +65,7 @@ if LANGUAGE_FINETUNE:
 print("Started Training")
 
 train['labels'] = encode(train["labels"])
-test['labels'] = encode(test["labels"])
+# test['labels'] = encode(test["labels"])
 
 test_sentences = test['text'].tolist()
 test_preds = np.zeros((len(test), args["n_fold"]))
@@ -114,7 +114,7 @@ else:
 model.save_model()
 
 test['predictions'] = decode(test['predictions'])
-test['labels'] = decode(test['labels'])
+# test['labels'] = decode(test['labels'])
 
 # time.sleep(5)
 
