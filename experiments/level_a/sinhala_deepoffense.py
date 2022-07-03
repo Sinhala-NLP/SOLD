bd36e1df7621f6b82fed2eadf6efe254bde4be72
isuri97 <you@example.com>
2022-07-04 04:16:35 +0530

    Remove evaluations

--- experiments/level_a/sinhala_deepoffense.py
+++ experiments/level_a/sinhala_deepoffense.py
@@ -65,7 +65,7 @@ if LANGUAGE_FINETUNE:
 print("Started Training")
 
 train['labels'] = encode(train["labels"])
-test['labels'] = encode(test["labels"])
+# test['labels'] = encode(test["labels"])
 
 test_sentences = test['text'].tolist()
 test_preds = np.zeros((len(test), args["n_fold"]))
@@ -114,10 +114,10 @@ else:
 model.save_model()
 
 test['predictions'] = decode(test['predictions'])
-test['labels'] = decode(test['labels'])
+# test['labels'] = decode(test['labels'])
 
 # time.sleep(5)
 
-print_information_multi_class(test, "predictions", "labels")
+# print_information_multi_class(test, "predictions", "labels")
 # print_information_multi_class(test, "predictions")
 test.to_csv(os.path.join(TEMP_DIRECTORY, RESULT_FILE), header=True, sep='\t', index=False, encoding='utf-8')
