d2c36d46a5e80c24d5de1d5a5c145d3e3e8c44db
isuri97 <you@example.com>
2022-07-15 16:13:30 +0530

    initialising the pipeline

--- experiments/level_a/sinhala_deepoffense.py
+++ experiments/level_a/sinhala_deepoffense.py
@@ -123,9 +123,6 @@ if (arguments.test is None):
 
     # time.sleep(5)
     print_information_multi_class(test, "predictions", "labels")
-
-    print_information_multi_class(test, "predictions")
-
     test.to_csv(os.path.join(TEMP_DIRECTORY, RESULT_FILE), header=True, sep='\t', index=False, encoding='utf-8')
 
 else:
