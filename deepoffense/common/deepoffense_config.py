bd458fcc62dcee31e734d8ed5c642e5f26204693
isuri97 <you@example.com>
2022-07-19 02:09:10 +0530

    initialising the pipeline

--- deepoffense/common/deepoffense_config.py
+++ deepoffense/common/deepoffense_config.py
@@ -23,7 +23,7 @@ args = {
     'fp16': False,
     'fp16_opt_level': 'O1',
     'max_seq_length': 128,  # 128
-    'train_batch_size':8 ,
+    'train_batch_size':64 ,
     'gradient_accumulation_steps': 1,
     'eval_batch_size':64,
     'num_train_epochs': 3,
