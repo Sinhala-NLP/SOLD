5371433ee57f9d079de016990b26c1f3e70be1dc
Damith Premasiri <damithpremasiri@gmail.com>
2022-09-13 12:57:09 +0100

    change n-folds

--- experiments/level_a/sinhala_deepoffense.py
+++ experiments/level_a/sinhala_deepoffense.py
@@ -74,29 +74,20 @@ MODEL_TYPE = arguments.model_type
 cuda_device = arguments.cuda_device
 
 if args["evaluate_during_training"]:
-    for i in range(args["n_fold"]):
-        if os.path.exists(args['output_dir']) and os.path.isdir(args['output_dir']):
-            shutil.rmtree(args['output_dir'])
-        print("Started Fold {}".format(i))
-        torch.cuda.set_device(cuda_device)
-        model = ClassificationModel(MODEL_TYPE, MODEL_NAME, args=args, num_labels=3,
-                                    use_cuda=torch.cuda.is_available(),
-                                    cuda_device=cuda_device)  # You can set class weights by using the optional weight argument
-        train_df, eval_df = train_test_split(train, test_size=0.1, random_state=SEED * i)
-        model.train_model(train_df, eval_df=eval_df, macro_f1=macro_f1, weighted_f1=weighted_f1,
-                          accuracy=sklearn.metrics.accuracy_score)
+    if os.path.exists(args['output_dir']) and os.path.isdir(args['output_dir']):
+        shutil.rmtree(args['output_dir'])
+    torch.cuda.set_device(cuda_device)
+    model = ClassificationModel(MODEL_TYPE, MODEL_NAME, args=args,
+                                use_cuda=torch.cuda.is_available(),
+                                cuda_device=cuda_device)  # You can set class weights by using the optional weight argument
+    train_df, eval_df = train_test_split(train, test_size=0.1, random_state=SEED)
+    model.train_model(train_df, eval_df=eval_df, macro_f1=macro_f1, weighted_f1=weighted_f1,
+                      accuracy=sklearn.metrics.accuracy_score)
 
-        predictions, raw_outputs = model.predict(test_sentences)
-        test_preds[:, i] = predictions
-        print("Completed Fold {}".format(i))
-    # select majority class of each instance (row)
-    final_predictions = []
-    for row in test_preds:
-        row = row.tolist()
-        final_predictions.append(int(max(set(row), key=row.count)))
-    test['predictions'] = final_predictions
+    predictions, raw_outputs = model.predict(test_sentences)
+    test['predictions'] = predictions
 else:
-    model = ClassificationModel(MODEL_TYPE, MODEL_NAME, args=args, num_labels=3,
+    model = ClassificationModel(MODEL_TYPE, MODEL_NAME, args=args,
                                 use_cuda=torch.cuda.is_available(), cuda_device=cuda_device)
     model.train_model(train, macro_f1=macro_f1, weighted_f1=weighted_f1, accuracy=sklearn.metrics.accuracy_score)
     predictions, raw_outputs = model.predict(test_sentences)
