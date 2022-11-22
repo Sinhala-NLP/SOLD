a6594a9c8546337111f4241f1b5c5d322c1aeb72
TharinduDR <rhtdranasinghe@gmail.com>
2022-11-22 17:31:56 +0000

    013: Adding mudes

--- experiments/token_level/mudes_config.py
+++ experiments/token_level/mudes_config.py
@@ -13,7 +13,7 @@ sinhala_args = {
 
     'fp16': False,
     'fp16_opt_level': 'O1',
-    'max_seq_length': 150,  # 128
+    'max_seq_length': 120,  # 128
     'train_batch_size': 16,
     'gradient_accumulation_steps': 1,
     'eval_batch_size': 32,
